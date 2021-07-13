f408a7eca53e65300a3662475f424b57a0715b60
m-e-l-u-h-a-n <purushottam.tiwari.cd.cse19@itbhu.ac.in>
2021-07-13 20:44:13 +0530

    Add readme

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ var mongoose=require("mongoose");
 //DEFINING THE BOOK MODEL
 var bookSchema=new mongoose.Schema({
 	/*DEFINE the following attributes-
-    title, genre, author, description, rating (out of 5), mrp,
+    title, genre, author, description, rating (out of 5), mrp, available_copies(instances).
      */
 })
 module.exports=mongoose.model("Book",bookSchema);
